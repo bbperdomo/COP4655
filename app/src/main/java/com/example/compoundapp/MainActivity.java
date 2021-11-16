ee58a3c1be6c7e50b8b36b22c89a7c9630cc2732
bperdomo2016 <bperdomo2016@fau.edu>
2021-11-16 00:36:26 -0500

    added imports and speech code

--- app/src/main/java/com/example/compoundapp/MainActivity.java
+++ app/src/main/java/com/example/compoundapp/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.compoundapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private final int REQ_CODE = 100;
+    public String city;
+    public EditText inputField;
+    public String link="https://api.openweathermap.org/data/2.5/weather?appid=30f3f228b57af5bb8d5d2cd69bc93bcb&units=imperial&";
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    public String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+    public GPSTracker gps;
+    public boolean fromGPSBtn = false;
+    public boolean gpsGood=false;
+    public TextView jsonSuccess;
+    public static WeatherData weatherData;
+
+    public static WeatherData getWeatherData() {
+        return weatherData;
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        jsonSuccess = findViewById(R.id.jsonSuccess);
+        jsonSuccess.setText("");
+
+        inputField = findViewById(R.id.userInput);
+
+        ImageView speak = findViewById(R.id.speak);
+        speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Need to speak");
+                try {
+                    startActivityForResult(intent, REQ_CODE);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry your device not supported",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
