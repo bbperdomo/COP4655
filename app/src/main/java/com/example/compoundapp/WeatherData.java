11a75335fd8ed9afee7a5ff086fbe03bd303b049
bperdomo2016 <bperdomo2016@fau.edu>
2021-11-16 01:19:20 -0500

    define weather parameters, used serialization

---
+++ app/src/main/java/com/example/compoundapp/WeatherData.java
@@ -0,0 +1,18 @@
+package com.example.compoundapp;
+
+import java.io.Serializable;
+
+public class WeatherData implements Serializable {
+    public String city;
+    public String temperature;
+    public String tempFeels;
+    public String minTemp;
+    public String maxTemp;
+    public String cloudiness;
+    public String pressure;
+    public String humidity;
+    public String windSpeed;
+    public String windDeg;
+    public String lat;
+    public String lon;
+}
