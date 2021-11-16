56f8dbdf5a2605c7798616b451d58038bed8bacb
bperdomo2016 <bperdomo2016@fau.edu>
2021-11-16 01:30:53 -0500

    handles map functionality, utilizes canvas code

---
+++ app/src/main/java/com/example/compoundapp/Map.java
@@ -0,0 +1,98 @@
+package com.example.compoundapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
+import com.google.android.gms.maps.model.UrlTileProvider;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Map extends FragmentActivity implements OnMapReadyCallback {
+
+    public static WeatherData wd;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(mainActivity);
+                        break;
+                    case R.id.action_results:
+                        Intent results = new Intent(getApplicationContext(), Results.class);
+                        startActivity(results);
+                        break;
+                    case R.id.action_map:
+                        Intent map = new Intent(getApplicationContext(), Map.class);
+                        startActivity(map);
+                        break;
+                    
+                }
+                return true;
+            }
+        });
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        TileProvider tileProvider = new UrlTileProvider(256, 256) {        @Override
+        public URL getTileUrl(int x, int y, int zoom) {            /* Define the URL pattern for the tile images */
+            String s = String.format("https://tile.openweathermap.org/map/temp_new/%d/%d/%d.png?appid=30f3f228b57af5bb8d5d2cd69bc93bcb", zoom, x, y);
+            if (!checkTileExists(x, y, zoom)) {
+                return null;
+            }            try {
+                return new URL(s);
+            } catch (MalformedURLException e) {
+                throw new AssertionError(e);
+            }
+        }        /*
+         * Check that the tile server supports the requested x, y and zoom.
+         * Complete this stub according to the tile range you support.
+         * If you support a limited range of tiles at different zoom levels, then you
+         * need to define the supported x, y range at each zoom level.
+         */
+            private boolean checkTileExists(int x, int y, int zoom) {
+                int minZoom = 12;
+                int maxZoom = 16;            return (zoom >= minZoom && zoom <= maxZoom);
+            }
+        };
+        double lat=0;
+        double lon=0;
+        WeatherData wdtemp = MainActivity.weatherData;
+        if(wdtemp!=null) {
+            wd = wdtemp;
+        }
+        if(wd!=null) {
+            lat = Double.parseDouble(wd.lat);
+            lon = Double.parseDouble(wd.lon);
+        }
+        LatLng loc = new LatLng(lat,lon);
+        googleMap.addMarker(new MarkerOptions().position(loc));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15.0f));
+        TileOverlay tileOverlay = googleMap.addTileOverlay(new TileOverlayOptions()
+                .tileProvider(tileProvider));
+    }
+
+}
